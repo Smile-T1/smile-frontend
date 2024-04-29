4e9f08706e896e3a2cb38e86dac79c2bf187269f
omar-adel1 <omaradelhassan12@gmail.com>
2024-04-30 02:50:01 +0300

    Remove Admin Dashboard routes and components; update App function to use only Home page route
    This commit removes all references to Admin Dashboard routes and components, including Dashboard, MainDashboard, Appointment, Patients, Schedule,

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "./Admin/layout/dashboard";
-import MainDashboard from "./Admin/dashboard";
-import Appointment from "./Admin/appointments";
-import Patients from "./Admin/patients";
-import Schedule from "./Admin/schedule";
-import Signup from './Components/Signup/Signup.jsx';
-import Login from './Components/Login/Login.jsx';
+// import Dashboard from "./Admin/layout/dashboard";
+// import MainDashboard from "./Admin/dashboard";
+// import Appointment from "./Admin/appointments";
+// import Patients from "./Admin/patients";
+// import Schedule from "./Admin/schedule";
+// import Signup from './Components/Signup/Signup.jsx';
+// import Login from './Components/Login/Login.jsx';
 import Home from './Pages/Home/Home.jsx';
-
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route
-          path="/mainDashboard"
-          element={
-            <Dashboard name="Dashboard">
-              <MainDashboard />
-            </Dashboard>
-          }
-        />
-        <Route
-          path="/appointment"
-          element={
-            <Dashboard name="Appointments">
-              <Appointment />
-            </Dashboard>
-          }
-        />
-        <Route
-          path="/patients"
-          element={
-            <Dashboard name="Patients">
-              <Patients />
-            </Dashboard>
-          }
-        />
-        <Route
-          path="/schedule"
-          element={
-            <Dashboard name="Schedule">
-              <Schedule />
-            </Dashboard>
-          }
-        />
-       <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="" element={<Home/>} />
-
-      </Routes>
-    </>
-=======
 import Sidebar from "./Components/Sidebar/Sidebar.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import Home from "./Pages/Home/Home.jsx";
 import Dashboard_Patient from "./Pages/Patient/Dashboard/Dashboard.jsx";
 import AdminDashboard from "./Pages/Admin/Dashboard/AdminDashboard.jsx";
 import Registration_Patient from "./Pages/Patient/Registration_Edit_info/Registration_Edit_info.jsx";
