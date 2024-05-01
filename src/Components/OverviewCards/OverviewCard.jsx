2ff9f7565aea2eb6c5fca8bd4de1b4e10e7ec754
Hassan <htarek1011@gmail.com>
2024-05-01 23:42:43 +0300

    Update component props in OverviewCard and AdminDashboard files

--- src/Components/OverviewCards/OverviewCard.jsx
+++ src/Components/OverviewCards/OverviewCard.jsx
@@ -2,11 +2,11 @@ import react from "react";
 import "./OverviewCard.css";
 import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 
-function Overviewcard() {
+function Overviewcard(props) {
   return (
     <div className="Overview-card">
       <Card id="card2">
-        <CardHeader>Total patients</CardHeader>
+        <CardHeader>{props.header}</CardHeader>
         <CardBody>
           <p id="body">300</p>
         </CardBody>
