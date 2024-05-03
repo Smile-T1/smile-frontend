581f43ccf4bfd215eeaa5b3e6c62b0b2e0ae778f
ahmedelmasry2002 <ahmedmkader55@gmail.com>
2024-05-03 22:48:07 +0300

    added new files

---
+++ public/src/Components/NewRequestCard/NewRequestCard.jsx
@@ -0,0 +1,22 @@
+import react from "react";
+import "./NewRequestCard.css";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+
+//make a doted card for new request
+function NewRequestCard() {
+  return (
+    <div className="NewRequest-card">
+      <Card id="new">
+        <CardBody>
+          <p id="body-name">Patient: John Doe</p>
+          <div className="bellow">
+            <p id="reason">Surgery</p>
+            <p id="body-DT">Date: 12/12/2021, 10:00 AM</p>
+          </div>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+export default NewRequestCard;
---
+++ public/src/Components/OverviewCards/OverviewCard.jsx
@@ -0,0 +1,18 @@
+import react from "react";
+import "./OverviewCard.css";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+
+function Overviewcard() {
+  return (
+    <div className="Overview-card">
+      <Card id="card2">
+        <CardHeader>Total patients</CardHeader>
+        <CardBody>
+          <p id="body">300</p>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+export default Overviewcard;
