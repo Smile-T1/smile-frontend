10e7482761a9f7e95e05831c2f623b73e1c3c5d1
Hana Fares <hanafares25@gmail.com>
2024-05-11 02:37:08 +0300

    login integrated

--- src/Components/Login/LoginEndpoints.js
+++ src/Components/Login/LoginEndpoints.js
@@ -1,5 +1,6 @@
 const VITE_SERVER_HOST = import.meta.env.VITE_SERVER_HOST;
 
+
 const loginUser = async (username, password) => {
     const url = `${VITE_SERVER_HOST}/api/auth/login`;
     const data = {
@@ -24,10 +25,13 @@ const loginUser = async (username, password) => {
     const id = responseData._id;
     const userUsername = responseData.username; // Renamed variable
     const profilePic = responseData.profilePic;
+    const userAccess = responseData.userAccess; // Extracts the "userAccess" field from the response and assigns it to the variable "userAccess"
 
     console.log('User ID:', id);
-    console.log('Username:', userUsername); // Updated variable name
+    console.log('Username:', userUsername); 
     console.log('Profile Picture:', profilePic);
+    console.log('User Access:', userAccess); 
+    
 
     return responseData;
   };
