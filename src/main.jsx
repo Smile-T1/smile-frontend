5f36d3933f42f7dd741eaa2421aae4343d221285
ahmedelmasry2002 <102183755+ahmedelmasry2002@users.noreply.github.com>
2024-05-14 22:43:50 +0300

    fix : error

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
+import { DataProvider } from "./context/Context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>{" "}
-    </ChakraProvider>
+    <DataProvider>
+      <ChakraProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChakraProvider>
+    </DataProvider>
   </React.StrictMode>
 );
