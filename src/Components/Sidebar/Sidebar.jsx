0df3de9af059442c8c84cea321f66736d2b1e4c9
omar-adel1 <omaradelhassan12@gmail.com>
2024-05-15 16:58:50 +0300

    LoginEndpoints.js: Removed a superfluous newline Sidebar.jsx: Removed unnecessary sticky position modifier index.css: Reordered at-rules for readability
    (No changes were made to

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -31,7 +31,7 @@ function Sidebar() {
 
   return (
     <div className='sidebar-portal'>
-      <aside className='content-wrapper-sidebar bg-white h-screen sticky top-0 p-2 min-w-[50px] dashboard-sidebar block  w-1/5'
+      <aside className='content-wrapper-sidebar bg-white h-screen sticky top-0 p-2 min-w-[50px] dashboard-sidebar block'
         style={{
           position: 'relative'
         }
