071c1b4c657f0c350a7ec82df3f71c9d396e84af
omar-adel1 <omaradelhassan12@gmail.com>
2024-05-16 20:23:39 +0300

    "Updated Book_app_Patient and Table_Data components, added getAllApointments endpoint, and modified handleBooking function to accept multipart/form-data"

--- src/Pages/Patient/PatientPortalEndPoints.js
+++ src/Pages/Patient/PatientPortalEndPoints.js
@@ -3,25 +3,22 @@ const serverHost = import.meta.env.VITE_SERVER_HOST;
 
 
 export async function handleBooking(appointmentData) {
-    console.log(appointmentData)
     try {
         const response = await axios.post(`${serverHost}/api/patient/appointment`, appointmentData, {
             headers: {
-                "Content-Type": "application/json",
+                "Content-Type": "multipart/form-data", 
                 'Authorization': `Bearer ${localStorage.getItem('token')}`
             }
         });
         console.log('response', response)
         if (response.status === 200) {
-            console.log("Appointment booked successfully");
-        } else {
-            console.error("Unexpected response:", response.status, response.statusText);
+            return { message: "Appointment booked successfully", status: "success" };
         }
     } catch (error) {
         if (error.response && error.response.status === 400) {
-            console.error("Doctor has appointment in the same time");
+            return { message: "Doctor has appointment in the same time", status: "error" };
         } else if (error.response && error.response.status === 500) {
-            console.error("Internal server error in booking appointment");
+            return { message: "Internal server error in booking appointment", status: "error" };
         }
         else {
             console.error("Error:", error.message);
@@ -37,6 +34,20 @@ export async function getDoctorsWithSpeciality(appointmentfor) {
             }
         });
         return response.data.doctors;
+    } catch (error) {
+        console.error("Error:", error.message);
+        throw error;
+    }
+}
+
+export async function getAllApointments() {
+    try {
+        const response = await axios.get(`${serverHost}/api/patient/allAppointments`, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        return response.data;
     } catch (error) {
         console.error("Error:", error.message);
         throw error;
