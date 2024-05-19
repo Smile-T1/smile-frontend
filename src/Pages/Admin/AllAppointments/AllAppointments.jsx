a70dae2d6bb80c2bdbbcebfec1410be49a93ed34
Hassan <htarek1011@gmail.com>
2024-05-19 13:47:35 +0300

    refactor: Update AllAppointments.jsx to fetch appointments from server
    This commit updates the AllAppointments.jsx file to fetch appointments from the server using the VITE_SERVER_HOST environment variable. It also adds authorization headers to the fetch request and handles errors during the fetch operation.

--- src/Pages/Admin/AllAppointments/AllAppointments.jsx
+++ src/Pages/Admin/AllAppointments/AllAppointments.jsx
@@ -3,15 +3,37 @@ import { useEffect, useState } from "react";
 import Table_Data from "../../../Components/Table_Data/Table_Data";
 import "./AllAppointments.css";
 import Header_Pages from "../../../Components/Header_Pages/Header_Pages";
+const VITE_SERVER_HOST = import.meta.env.VITE_SERVER_HOST;
 
 function AllAppointments() {
   const [appointments, setAppointments] = useState([]);
 
-  //   useEffect(() => {
-  //     fetch("http://localhost:3001/appointments")
-  //       .then((response) => response.json())
-  //       .then((data) => setAppointments(data));
-  //   }, []);
+  useEffect(async () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login");
+    }
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${VITE_SERVER_HOST}/api/admin/appointments`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        const data = await response.json();
+        // console.log(data.appointments);
+        // setAppointments(data.appointments);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    await fetchData();
+  }, []);
 
   return (
     <div className="appointment_admin">
