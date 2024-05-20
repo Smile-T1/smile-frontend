77fec7c499f5166de83f24060b961f9e62ac6ed6
omar-adel1 <omaradelhassan12@gmail.com>
2024-05-20 07:43:23 +0300

    "Updated Settings.jsx to import getSettings function, added userData state, and passed user data to ProfileCard and InfoCard components."

--- src/Pages/Settings/Settings.jsx
+++ src/Pages/Settings/Settings.jsx
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import "./Settings.css";
 import Header_Pages from '../../Components/Header_Pages/Header_Pages';
 import ProfileCard from '../../Components/Settings/ProfileCard/ProfileCard';
 import InfoCard from '../../Components/Settings/InfoCard/InfoCard';
 import PasswordCard from '../../Components/Settings/PasswordCard/PasswordCard';
+import { getSettings } from "../../Pages/Patient/PatientPortalEndPoints";
 
 function Settings() {
   const [selectedCard, setSelectedCard] = useState('Info');
+  const [userData, setUserData] = useState([]);
   const navigate = useNavigate();
+
   useEffect(() => {
     const token = localStorage.getItem('token');
-    if (token === undefined || token === null) {
+    if (!token) {
       navigate('/login');
     }
 
@@ -33,15 +36,30 @@ function Settings() {
 
   return (
     <div className='Dashboard-patient-portal'>
-      <Header_Pages type='Patient' header='Settings'/>
+      <Header_Pages type='Patient' header='Settings' />
       <div className='settings-main-container-page'>
-        <ProfileCard onCardSelect={handleCardSelection} />
-        {selectedCard === "Info" && <InfoCard />}
+        <ProfileCard
+          onCardSelect={handleCardSelection}
+          firstName={userData?.patient?.firstName}
+          lastName={userData?.patient?.lastName}
+          email={userData?.patient?.email}
+          mobile={userData?.patient?.mobile[0]}
+          profilePic={userData?.patient?.profilePic}
+        />
+        {selectedCard === "Info" &&
+          <InfoCard
+            firstName={userData?.patient?.firstName}
+            lastName={userData?.patient?.lastName}
+            username={userData?.patient?.username}
+            mobile={userData?.patient?.mobile[0]}
+            email={userData?.patient?.email}
+            address={userData?.patient?.address}
+            profilePic={userData?.patient?.profilePic}
+          />}
         {selectedCard === "Password" && <PasswordCard />}
       </div>
     </div>
   )
 }
 
-export default Settings
-
+export default Settings;
