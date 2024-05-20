4cc3353bf8e82a8976b246f3dea0eb3112c4c76c
Hana Fares <hanafares25@gmail.com>
2024-05-20 17:52:19 +0300

    contact us and meet the team

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,17 +6,20 @@ import waves from "../../assets/waves.png";
 import location from "../../assets/location.png";
 import date from "../../assets/date.png";
 import time from "../../assets/time.png";
-import logo from "../../assets/Smile.png";
+import logo from "../../assets/Smile_without.png";
 import doctor from "../../assets/doctor.png";
 import about from "../../assets/about.png";
-// import ActiveIcon from "./icons/activeIcon";
+import Omar from "../../assets/Omar.jpeg";
+import Ahmed from "../../assets/abdelqader.jpeg"
+import Hassan from "../../assets/Hassan.jpeg"
+import Hana from "../../assets/hana.jpeg"
 
 function Home(props) {
   const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState('home');
 
   useEffect(() => {
-    localStorage.removeItem('token');
+    localStorage.removeItem('token'); 
   }, []);
 
   const handleTabChange = (index) => {
@@ -41,36 +44,24 @@ function Home(props) {
           <img src={waves} alt="background" /> </div>}
       {activeTab === 'home' && <div className='eclipsehome1'></div>}
       {activeTab === 'home' && <div className='eclipsehome2'></div>}
+      {activeTab === 'about' &&
+        <div className='about-waves'>
+          <img src={waves} alt="background" /> </div>}
+      
       <div className={`upper-rectangle ${activeTab}`}>
+
+        <div className='logo'>
+          <img src={logo} alt="" />
+          <span className='logo-name'>Smile Clinic</span>
+        </div>
         <div className='homeMenu'>
           <Tabs position='relative' variant='unstyled' onChange={handleTabChange}>
-            <div style={{ display: 'flex', gap: '322px', alignItems: 'center', justifyContent: 'center' }}>
-              <div className='logo' style={{ marginTop: '0', height: 'auto' }}>
-                <img src={logo} alt="Smile Clinic" style={{ height: '97px', width: '150px' }} />
-              </div>
-              <TabList className="homeTab">
-                <Tab onClick={() => setActiveTab('home')}
-                  className={`tab ${activeTab === 'home' ? 'active' : ''}`}>
-                  {/* {activeTab === 'home' && <ActiveIcon />} */}
-                  Home
-                </Tab>
-                <Tab onClick={() => setActiveTab('about')}
-                  className={`tab ${activeTab === 'about' ? 'active' : ''}`}>
-                  {/* {activeTab === 'about' && <ActiveIcon />} */}
-                  About Us
-                </Tab>
-                <Tab onClick={() => setActiveTab('contact')}
-                  className={`tab ${activeTab === 'contact' ? 'active' : ''}`} >
-                  {/* {activeTab === 'contact' && <ActiveIcon />} */}
-                  Contact
-                </Tab>
-                <Tab onClick={() => { navigate('/login') }}
-                  >
-                  {/* {activeTab === 'login' && <ActiveIcon />} */}
-                  Log In
-                </Tab>
-              </TabList>
-            </div>
+            <TabList>
+              <Tab onClick={() => setActiveTab('home')} >Home</Tab>
+              <Tab onClick={() => setActiveTab('about')}>About Us</Tab>
+              <Tab onClick={() => setActiveTab('contact')} >Contact</Tab>
+              <Tab onClick={() => { navigate('/login') }}>Log In</Tab>
+            </TabList>
             <TabPanels>
               <TabPanel>
                 <div className={`home ${activeTab}`}>
@@ -83,15 +74,14 @@ function Home(props) {
                       <img src={doctor} alt="doctor" />
                     </div>
                   </div>
-                  <Card>
-                    <CardBody>
+                  <div className='card1'>
                       <div className='card-title'>
                         <span>Smile</span>
                         <span> Confidence </span>
                         <span>Happiness</span>
                       </div>
                       <div className='card-text'>
-                        <div className='location'>
+                        <div className='homeloc'>
                           <img src={location} alt="location" />
                           <span> Location</span>
                         </div>
@@ -102,11 +92,34 @@ function Home(props) {
                         <div className='time'>
                           <img src={time} alt="time" />
                           <span> 10:00 AM</span>
-
                         </div>
                       </div>
-                    </CardBody>
-                  </Card>
+                      </div>
+                 <div className='meetteam'>
+                <h3>Meet the team</h3>
+                <div className='team-cards'>
+                  <div className='card'>
+                    <img src={Omar} alt="Eng Omar Adel" />
+                    <h4>Dr. Omar Adel</h4>
+                      <h6>Orthodontist</h6>
+                  </div>
+                  <div className='card'>
+                  <img src={Ahmed} alt="Eng Ahmed Abdelqader" />
+                  <h4>Dr. Ahmed Abdelqader</h4>
+                  <h6>Oral Pathologist</h6>
+                  </div>
+                  <div className='card'>
+                  <img src={Hassan} alt="Eng Hassan Mohamed" />
+                  <h4>Dr. Hassan Mohamed</h4>
+                  <h6>Endodontist</h6>
+                  </div>
+                  <div className='card'> 
+                  <img src={Hana} alt="Eng Hana Fares" />
+                  <h4>Dr. Hana Fares</h4>
+                  <h6>Pediatric Dentist</h6>
+                  </div>
+                </div>
+              </div>
                 </div>
               </TabPanel>
               <TabPanel>
@@ -119,6 +132,7 @@ function Home(props) {
                     <div className='bookapp'>
                       <button className='bookbutton'>Book today</button>
                     </div>
+
                     <div className='about-bottom'>
                       <div className='abouttxt'>
                         <div className='about-text4'>
@@ -126,51 +140,75 @@ function Home(props) {
                         </div>
                         <div className='about-text5'>
                           We are a dedicated team of eye care professionals committed to providing our patients with the highest quality of care. Our mission is to help our patients maintain healthy eyes and clear vision throughout their lives. <br /> <br />
-                          Our clinic offers a comprehensive range of services, including routine eye exams, vision screenings, and treatment for a variety of eye conditions and diseases. Our skilled and experienced doctors use the latest technologies and techniques to ensure that our patients receive the best possible care.
-                        </div>
-                      </div>
-                      <img src={about} alt="dental" />
-                    </div>
-                  </div>
 
-                </div>
-              </TabPanel>
-              <TabPanel>
-                <div className='contactform'>
-                  <h2>Contact us</h2>
-                  <form>
-                    <div className='formName'>
-                      <label>
-                        Name:
-                        <input type="text" name="name" />
-                      </label>
-                    </div>
-                    <div className='formEmail'>
-                      <label>
-                        Email:
-                        <input type="email" name="email" />
-                      </label>
-                    </div>
-                    <div className='formPhone'>
-                      <label>
-                        Phone:
-                        <input type="tel" name="phone" />
-                      </label>
-                    </div>
-                    <div className='formMessage' >
-                      <label>
-                        Message:
-                        <input name="message" />
-                      </label>
-                    </div>
-                    <div className='formSubmit'>
-                      <button>Submit</button>
-                    </div>
-                  </form>
-                </div>
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
+Our clinic offers a comprehensive range of services, including routine eye exams, vision screenings, and treatment for a variety of eye conditions and diseases. Our skilled and experienced doctors use the latest technologies and techniques to ensure that our patients receive the best possible care.
+        </div>
+        </div>
+        <img src={about} alt="dental" />
+      </div>
+    </div>
+     
+</div>
+    </TabPanel>
+    <TabPanel>
+      <div className='contactus'>
+      <div className='contactform'>
+    <h2>Contact us</h2>
+  <form>
+    <div className='formName'>
+    <label>
+      Name:
+      <input type="text" name="name" />
+    </label>
+    </div>
+    <div className='formEmail'>
+    <label>
+      Email:
+      <input type="email" name="email"  />
+    </label>
+    </div>
+    <div  className='formPhone'>
+    <label>
+      Phone:
+      <input type="tel" name="phone" />
+    </label>
+    </div>
+    <div className='formMessage' >
+    <label>
+      Message:
+      <input  name="message" />
+    </label>
+    </div>
+    <div className='formSubmit'>
+    <button>
+      Submit
+    </button>
+    </div>
+  
+  </form>
+  </div>
+  <div className='workinghours'>
+      <div className='hours'>
+      <h3>Working Hours</h3>
+      <b>Monday - Friday: <br /> 8:00 AM - 5:00 PM</b> <br />
+      <b>Saturday: <br /> 8:00 AM - 12:00 PM</b>
+      </div>
+      <div className='location'>
+      <h3>Locations</h3>
+      <b>Smile Clinic - Nasr City</b> <br />
+      <b>Smile Clinic - Maadi</b>
+      </div>
+      <div className='contphone'>
+        <h3>Phone Numbers</h3>
+        <b>01000000000</b> <br />
+        <b>02-26891638</b>
+      </div>
+
+    </div>
+    </div>
+</TabPanel>
+  </TabPanels>
+</Tabs>
         </div>
       </div>
 
